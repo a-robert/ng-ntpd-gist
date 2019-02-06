476078e7b3a23564ac7588b5c510f15b09c4e0b3
Robert Asaturyan <robert.asaturyan@macadamian.com>
2019-02-06 11:47:15 +0400

    Added bootstrap for the sake of the god

--- src/core/app.module.ts
+++ src/core/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app.component';
+
+const coreModules = [
+  NgbModule,
+  BrowserModule
+];
+
+const appModules = [
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    ...coreModules,
+    ...appModules,
+    AppRoutingModule // Must be last for proper routing
   ],
   providers: [],
   bootstrap: [AppComponent]
